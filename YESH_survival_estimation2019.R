18a7ac7f93dfab9c1dc72b204b5a9319917d7fc6
Steffen <steffen.oppel@gmail.com>
2019-10-28 17:00:09 +0000

    updated JAGS data input to 8 sites
    ensured that effort and ringing data are grouped by the 8 sites that Martin specified

--- YESH_survival_estimation2019.R
+++ YESH_survival_estimation2019.R
@@ -205,7 +205,9 @@ YESH<- yesh %>% #mutate(Colony=caves$Subcolony_Name[match(Cave_String,caves$Cave
            group_by(SITE, OCC_NR,ringnumber) %>%   ## REPLACED Colony with SITE based on Martin's grouping in October 2019
            summarise(ALIVE=max(seen,na.rm=T)) %>%
            spread(key=OCC_NR, value=ALIVE) %>%
-           replace(is.na(.), 0)
+           replace(is.na(.), 0) %>%
+           ungroup() %>%
+           mutate(SITE=as.character(SITE))
 
 #length is longer the unique(records$ringnumber) cause some rings are assigned to more than one cavestring but this should not be a problem
 ## THESE TWO DIMENSIONS DO NOT MATCH, BECAUSE SOME BIRDS WERE RECORDED IN >1 SITE
@@ -234,6 +236,7 @@ transientCH<-transients %>% gather(key='OCC', value='surv',-ringnumber,-SITE) %>
 YESH<- YESH %>% dplyr::filter(!(ringnumber %in% duplicates)) %>%
   bind_rows(transientCH)
 dim(YESH)
+
 
 
 #########################################################################
@@ -326,270 +329,153 @@ dim(zinit)
 ## STANDARDISE HOURS TO AVOID INVALID PARENT ERROR IN JAGS
 COLEFF<- eff %>% group_by(SITE, OCC_NR) %>%
          summarise(effort=sum(effort)) %>%
-         mutate(effort=scale(effort, center=mean(effort))/sd(effort)) %>%
+         mutate(effort=scale(effort, center=T, scale=T)) %>% #mean(effort))/sd(effort)) %>%
          spread(key=OCC_NR, value=effort) %>%
+         replace(is.na(.), 0) %>%
+         ungroup() %>%
+         mutate(COL_NR=seq_along(SITE))
+               
+effmat<-as.matrix(COLEFF[,2:9], dimnames=F)
+               
+               
+               
+               
+## CREATE LOOKUP VECTOR FOR WHICH COLONY BIRDS WERE CAUGHT IN
+head(YESH)
+colvec<-COLEFF$COL_NR[match(YESH$SITE,COLEFF$SITE)]
+               
+length(f)
+length(colvec)
+               
+## CREATE LOOKUP VECTOR FOR WHICH COLONY BIRDS WERE CAUGHT IN
+DURMAT<- survPeriods %>% group_by(SITE, OCC_NR) %>%
+         summarise(int=sum(surv_int)) %>%
+         spread(key=OCC_NR, value=int) %>%
          replace(is.na(.), 0)
-         COLEFF$COL_NR<-1#seq(1:5)
                
-effmat<-as.matrix(COLEFF[,2:7], dimnames=F)
-               
+periods<-as.matrix(DURMAT[,2:9], dimnames=F)
                
                
                
                
                
+##########################################################################################################
+# Specify JS model with colony-occasion-specific recapture probability and random time effects
+##########################################################################################################
                
-               ## CREATE LOOKUP VECTOR FOR WHICH COLONY BIRDS WERE CAUGHT IN
-               
-               
-               
-               head(YESH)
-               
-               colvec<-COLEFF$COL_NR[match(YESH$Colony,COLEFF$Colony)]
-               
-               
-               
-               length(f)
-               
-               length(colvec)
-               
-               
-               
-               
-               
-               ## CREATE LOOKUP VECTOR FOR WHICH COLONY BIRDS WERE CAUGHT IN
-               
-               
-               
-               DURMAT<- survPeriods %>% group_by(Colony, OCC_NR) %>%
-               
-               summarise(int=sum(surv_int)) %>%
-               
-               spread(key=OCC_NR, value=int) %>%
-               
-               replace(is.na(.), 0)
-               
-               
-               
-               periods<-as.matrix(DURMAT[,2:7], dimnames=F)
-               
-               
-               
-               
-               
-               
-               
-               ##########################################################################################################
-               
-               # Specify JS model with colony-occasion-specific recapture probability and random time effects
-               
-               ##########################################################################################################
-               
-               
-               
-               setwd("C:\\STEFFEN\\RSPB\\Malta\\Analysis\\Survival_analysis\\YESH_survival")
-               
-               sink("YESH_JS_abundance_survival_simp.jags")
-               
-               cat("
-               
-               
-               
-               model
-               
-               {
+setwd("C:\\STEFFEN\\RSPB\\Malta\\Analysis\\Survival_analysis\\Yelkouan")
+sink("YESH_JS_abundance_survival_simp.jags")
+cat("
+ model
+   {
+   # Priors and constraints
                  
+   ### SURVIVAL PROBABILITY
+   mean.phi ~ dunif(0.95, 1)                      # Priors for age-specific DAILY survival - this is the basic survival that is scaled to difference between occasions
+   #mean.p ~ dbeta(1.5, 4)                          # Prior for mean recapture switched to beta from unif
+   #logit.p <- log(mean.p / (1-mean.p))           # Logit transformation
+   #beta.effort ~ dnorm(0,0.01)                   # Prior for trapping effort offset on capture probability
                  
-                 
-                 # Priors and constraints
-                 
-                 
-                 
-                 ### SURVIVAL PROBABILITY
-                 
-                 
-                 
-                 mean.phi ~ dunif(0.95, 1)                      # Priors for age-specific DAILY survival - this is the basic survival that is scaled to difference between occasions
-                 
-                 #mean.p ~ dbeta(1.5, 4)                          # Prior for mean recapture switched to beta from unif
-                 
-                 #logit.p <- log(mean.p / (1-mean.p))           # Logit transformation
-                 
-                 #beta.effort ~ dnorm(0,0.01)                   # Prior for trapping effort offset on capture probability
-                 
-                 
-                 
-                 for (i in 1:M){
-                   
-                   for (t in 1:(n.years-1)){
-                     
-                     beta.int[i,t] <- pow(mean.phi,periods[t])       ### exponentiates daily survival by the length between capture sessions
-                     
-                     mu[i,t] <- log(beta.int[i,t] / (1-beta.int[i,t]))       # Logit transformation
-                     
-                     logit(phi[i,t]) <- mu[i,t] + surv.raneff[t]
-                     
-                   } #t
+   for (i in 1:M){
+      for (t in 1:(n.years-1)){
+          beta.int[i,t] <- pow(mean.phi,periods[t])       ### exponentiates daily survival by the length between capture sessions
+          mu[i,t] <- log(beta.int[i,t] / (1-beta.int[i,t]))       # Logit transformation
+          logit(phi[i,t]) <- mu[i,t] + surv.raneff[t]
+      } #t
                    
                    
                    
-                   ### RECAPTURE PROBABILITY
+       ### RECAPTURE PROBABILITY
                    
+       for (t in 1:n.years){
+           #logit(p[i,t]) <- logit.p + capt.raneff[i] ###beta.effort*effort[t] + 
+           p[i,t] ~ dbeta(1.5, 4) 
+       }
+    } #i
+                 
+                 
+    ## RANDOM TIME EFFECT ON SURVIVAL
+                 
+    for (t in 1:(n.years-1)){
+        surv.raneff[t] ~ dnorm(0, tau.surv)
+        }
+                 
+    #for (i in 1:M){
+    #  capt.raneff[i] ~ dnorm(0, tau.capt)
+    #}
+                 
+                 
+    ### PRIORS FOR RANDOM EFFECTS
+                 
+    sigma.surv ~ dunif(0, 10)                     # Prior for standard deviation of survival
+    tau.surv <- pow(sigma.surv, -2)
+    #sigma.capt ~ dunif(0, 10)                     # Prior for standard deviation of capture
+    #tau.capt <- pow(sigma.capt, -2)
+     
+
+
+
+            
+    ####### LIKELIHOOD #############
+                 
+    for (t in 1:n.years){
+         gamma[t] ~ dunif(0, 1)
+    } #t
+                 
+                 
+    # Likelihood
+                 
+     for (i in 1:M){
                    
+          # First occasion
+          # State process
+          z[i,1] ~ dbern(gamma[1])
                    
-                   for (t in 1:n.years){
-                     
-                     #logit(p[i,t]) <- logit.p + capt.raneff[i] ###beta.effort*effort[t] + 
-                     
-                     p[i,t] ~ dbeta(1.5, 4) 
-                     
-                   }
+          # Observation process
+          mu1[i,1] <- z[i,1] * p[i,1]
+          y[i,1] ~ dbern(mu1[i,1])
                    
-                   
-                   
-                 } #i
-                 
-                 
-                 
-                 
-                 
-                 ## RANDOM TIME EFFECT ON SURVIVAL
-                 
-                 for (t in 1:(n.years-1)){
-                   
-                   surv.raneff[t] ~ dnorm(0, tau.surv)
-                   
-                 }
-                 
-                 
-                 
-                 #for (i in 1:M){
-                 
-                 #  capt.raneff[i] ~ dnorm(0, tau.capt)
-                 
-                 #}
-                 
-                 
-                 
-                 
-                 
-                 ### PRIORS FOR RANDOM EFFECTS
-                 
-                 sigma.surv ~ dunif(0, 10)                     # Prior for standard deviation of survival
-                 
-                 tau.surv <- pow(sigma.surv, -2)
-                 
-                 
-                 
-                 #sigma.capt ~ dunif(0, 10)                     # Prior for standard deviation of capture
-                 
-                 #tau.capt <- pow(sigma.capt, -2)
-                 
-                 
-                 
-                 
-                 
-                 ####### LIKELIHOOD #############
-                 
-                 
-                 
-                 for (t in 1:n.years){
-                   
-                   gamma[t] ~ dunif(0, 1)
-                   
-                 } #t
-                 
-                 
-                 
-                 
-                 
-                 # Likelihood
-                 
-                 for (i in 1:M){
-                   
-                   # First occasion
-                   
+          # Subsequent occasions
+          for (t in 2:n.years){
                    # State process
-                   
-                   z[i,1] ~ dbern(gamma[1])
-                   
-                   
-                   
+                   #q[i,t-1] <- 1-z[i,t-1]		# Availability for recruitment
+                   recru[i,t-1] <- max(z[i,1:(t-1)])		# Availability for recruitment - this will be 0 if the bird has never been observed before and 1 otherwise
+                   pot.alive[i,t] <- phi[i,t-1] * z[i,t-1] + gamma[t] * (1-recru[i,t-1])
+                   z[i,t] ~ dbern(pot.alive[i,t])
+                     
                    # Observation process
-                   
-                   mu1[i,1] <- z[i,1] * p[i,1]
-                   
-                   y[i,1] ~ dbern(mu1[i,1])
-                   
-                   
-                   
-                   # Subsequent occasions
-                   
-                   for (t in 2:n.years){
+                   mu1[i,t] <- z[i,t] * p[i,t]	
+                   y[i,t] ~ dbern(mu1[i,t])
                      
-                     # State process
-                     
-                     #q[i,t-1] <- 1-z[i,t-1]		# Availability for recruitment
-                     
-                     recru[i,t-1] <- max(z[i,1:(t-1)])		# Availability for recruitment - this will be 0 if the bird has never been observed before and 1 otherwise
-                     
-                     pot.alive[i,t] <- phi[i,t-1] * z[i,t-1] + gamma[t] * (1-recru[i,t-1])
-                     
-                     z[i,t] ~ dbern(pot.alive[i,t])
-                     
-                     
-                     
-                     # Observation process
-                     
-                     mu1[i,t] <- z[i,t] * p[i,t]	
-                     
-                     y[i,t] ~ dbern(mu1[i,t])
-                     
-                   } #t
+           } #t
                    
-                 } #i 
+     } #i 
                  
                  
                  
                  
+    # DERIVED PARAMETERS TO REPORT POPULATION SIZE
                  
-                 # DERIVED PARAMETERS TO REPORT POPULATION SIZE
-                 
-                 for (t in 1:n.years){
-                   
-                   N[t] <- sum(z[1:M,t])        # Actual population size
-                   
-                 } #t
+    for (t in 1:n.years){
+           N[t] <- sum(z[1:M,t])        # Actual population size
+    } #t
                  
                  
                  
-                 for (i in 1:M){
-                   
-                   Nind[i] <- sum(z[i,1:n.years])
-                   
-                   Nalive[i] <- 1-equals(Nind[i], 0)
-                   
-                 } #i
+    for (i in 1:M){
+           Nind[i] <- sum(z[i,1:n.years])
+           Nalive[i] <- 1-equals(Nind[i], 0)
+    } #i
                  
                  
                  
-                 # DERIVED SURVIVAL PROBABILITIES PER YEAR 
+    # DERIVED SURVIVAL PROBABILITIES PER YEAR 
+    ann.surv <- pow(mean.phi,365)
                  
-                 ann.surv <- pow(mean.phi,365)
-                 
-                 
-                 
-               }								#### END OF THE FUNCTION LOOP
+}								#### END OF THE FUNCTION LOOP
                
-               
-               
-               
-               
-               
-               
-               ",fill = TRUE)
-               
-               sink()
+",fill = TRUE)
+sink()
                
                
                
